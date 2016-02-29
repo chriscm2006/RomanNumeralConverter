4c171c314e7ea8b3213de9dda0979e7b3f84f25e
Chris McMeeking <chris.mcmeeking@deque.com>
2016-02-29 14:50:43 -0500

    Initial Commit

---
+++ Roman Numeral ConverterTests/Roman_Numeral_ConverterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Roman_Numeral_ConverterTests.swift
+//  Roman Numeral ConverterTests
+//
+//  Created by Chris McMeeking on 2/29/16.
+//  Copyright © 2016 Chris McMeeking. All rights reserved.
+//
+
+import XCTest
+@testable import Roman_Numeral_Converter
+
+class Roman_Numeral_ConverterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
